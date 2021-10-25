d658dc31c83ace20b303b304438a2fb17e46c6d1
Nitha <alive1704@gmail.com>
2021-10-25 17:47:14 +0700

    add page access token facebook

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { GlobalService } from 'src/app/service/global.service';
 
+declare var FB: any;
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -21,6 +22,7 @@ export class ProfileComponent implements OnInit {
     if (!this.checkIsLogin) {
       this.router.navigate(['login']);
     }
+    this.getFBSDK();
   }
 
   onEdit() {
@@ -32,16 +34,69 @@ export class ProfileComponent implements OnInit {
   }
 
   saveInfo() {
-
     const inputs = document.querySelectorAll('.form-group input');
     inputs.forEach((input) => {
       input.setAttribute('value', input.getAttribute('value'));
-      input.setAttribute('disabled',"");
-      console.log(input.getAttribute('value'))
+      input.setAttribute('disabled', '');
+      console.log(input.getAttribute('value'));
     });
-
-
-
     this.isProfileEdit = false;
+  }
+
+  getFBSDK() {
+    (<any>window).fbAsyncInit = function () {
+      FB.init({
+        appId: '870578177158205',
+        cookie: true,
+        xfbml: true,
+        version: 'v12.0',
+      });
+
+      FB.AppEvents.logPageView();
+    };
+
+    (function (d, s, id) {
+      var js: any,
+        fjs: any = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
+  }
+
+  onLogin() {
+    FB.login(
+      (res: any) => {
+        console.log(res);
+        this.getFBData(res.authResponse.accessToken, res.authResponse.userID);
+      },
+      {
+        scope:
+          'public_profile, email, pages_messaging, pages_messaging_phone_number, pages_manage_ads, pages_manage_metadata, pages_read_engagement',
+        auth_type: 'rerequest',
+      }
+    );
+  }
+
+  getFBData(accessToken: any, userID: any) {
+    console.log('accessToken:', accessToken,
+    '\n' 
+    ,'userID:', userID);
+    FB.api(
+      '/' +
+        userID +
+        '?fields=id, first_name, last_name, middle_name, name, name_format, picture, short_name, accounts, email',
+      (res: any) => {
+        if (res && !res.error) {
+          console.log(res);
+        } else {
+          console.log(res);
+        }
+      }
+    );
   }
 }
